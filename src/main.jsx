a4f9999acbdd3a9fce706273bfae09b5d9d67be1
naitik00101 <naitikmorkar@gmail.com>
2026-02-24 12:48:28 +0530

    feat: add vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
@@ -7,6 +8,8 @@ import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
 import App from "./App";
 import "./index.css";
+
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
